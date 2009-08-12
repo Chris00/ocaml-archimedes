f14434eaa74ff06af4a1d6e4fd19747689043eb1
Bertrand Desmons <bibi-6@hotmail.fr>
2009-08-12 15:09:59 +0200

    Modified the way of treating labels (now with an iterator). print_axes in interface. Commented the interface.

--- src/axes.ml
+++ src/axes.ml
@@ -34,8 +34,8 @@ let make_axis major minor loc mode data =
 let make axes x y =
   {axes = axes; x = x; y = y;}
 
-let print_axes t ~xmin ~xmax ~ymin ~ymax ch =
-  match t.axes with
+let print_axes axes ~xmin ~xmax ~ymin ~ymax ch =
+  match axes with
     `Rectangle(bx, by) ->
       let x, y = min xmin xmax, min ymin ymax in
       let w, h = abs_float (xmax -. xmin), abs_float (ymax -. ymin) in
@@ -61,41 +61,58 @@ let get_funct loc  = match loc with
   | `Logarithmic -> (fun n m i ->
       log (1. +. 9. *. (float i) /. (float (n * m))), i mod m = 0)
 
-let get_text loc data vmin vmax n i =
+exception Inner_error
+
+let get_labels data =
   match data with
-    `Numbers ->
-      let x = vmin +. (vmax -. vmin) *. (float i) /. (float n) in
-      Printf.sprintf "%f"
-        (match loc with
-         | `Linear -> x
-         | `Logarithmic -> 10.**x)
+    `Numbers -> raise Inner_error
   | `Other list ->
-      List.nth list i
+      let u = ref list in
+      fun () -> match !u with
+        [] -> failwith "No labels"
+      | s::l -> u := l; s
 
 let print_tic ch tic =
   match tic with `P name ->
     C.render ch name
 
-let print_tics_normalized axis vmin vmax x_axis ch =
+let print_tics_normalized axis vmin vmax x_axis
+    print_tic get_funct get_labels ch =
   let loc_tic = get_funct axis.loc in
   match axis.mode with
     Automatic -> failwith "NYI"
   | Fixed(major, minor) ->
       let n = (major - 1) * minor + 1 in
       let f = loc_tic major minor in
-      for i = 0 to n do
-        let v, is_major = f i in
-        C.move_to ch v 0.;
-        print_tic ch (if is_major then axis.major else axis.minor);
-        if is_major then
-          let rotate = if x_axis then 0. else (-2.) *. atan 1. in
-          let text = get_text axis.loc axis.data vmin vmax n i in
-          C.show_text ch rotate v 0.
-            (if x_axis then Backend.CB else Backend.LC) text
-      done
+      let next_label =
+        try
+          get_labels axis.data
+        with Inner_error ->
+          let i = ref 0 in
+          fun () ->
+            let x = vmin +. (vmax -. vmin) *. (float !i) /. (float n) in
+            incr i;
+            Printf.sprintf "%f"
+              (match axis.loc with
+               | `Linear -> x
+               | `Logarithmic -> 10.**x)
+      in
+      let rec maketic i =
+        if i <= n then
+          let v, is_major = f i in
+          C.move_to ch v 0.;
+          print_tic ch (if is_major then axis.major else axis.minor);
+          if is_major then
+            let rotate = if x_axis then 0. else (-2.) *. atan 1. in
+            C.show_text ch rotate v 0.
+              (if x_axis then Backend.CB else Backend.LC) (next_label ());
+            maketic (i+1)
+          else maketic (i+1)
+      in maketic 0
 
 
-let print_tics axis ~vmin ~vmax ~vinv x_axis ch =
+
+let print_tics axis ~vmin ~vmax ~vinv x_axis print_tic get_funct get_labels ch =
   C.save ch;
   if x_axis then (*vmin = xmin,...*)
     C.add_translate ch "~axis" vmin vinv
@@ -109,14 +126,17 @@ let print_tics axis ~vmin ~vmax ~vinv x_axis ch =
   C.set_coordinate ch "~axis";
   let diff = vmax -. vmin in
   C.scale ch diff diff;
-  print_tics_normalized axis vmin vmax x_axis ch;
+  print_tics_normalized axis vmin vmax x_axis print_tic get_funct get_labels ch;
   (*Restoring to previous coordinates.*)
-C.restore ch
+  C.restore ch
 
-let print t ~xmin ~xmax ~ymin ~ymax ch =
-  let x,y = print_axes t ~xmin ~xmax ~ymin ~ymax ch in
-  print_tics t.x ~vmin:xmin ~vmax:xmax ~vinv:y true ch;
-  print_tics t.x ~vmin:ymin ~vmax:ymax ~vinv:x false ch
+let print t ~xmin ~xmax ~ymin ~ymax ?(print_tic = print_tic)
+    ?(get_funct = get_funct) ?(get_labels = get_labels) ch =
+  let x,y = print_axes t.axes ~xmin ~xmax ~ymin ~ymax ch in
+  print_tics t.x ~vmin:xmin ~vmax:xmax ~vinv:y true
+    print_tic get_funct get_labels ch;
+  print_tics t.y ~vmin:ymin ~vmax:ymax ~vinv:x false
+    print_tic get_funct get_labels ch
 
 
 (*Local variables:*)
--- src/axes.mli
+++ src/axes.mli
@@ -11,34 +11,53 @@ type data = [ `Numbers | `Other of string list ]
 type ('a, 'b, 'c) axis
 (**The type storing all information of an axis.*)
 type ('a, 'b, 'c, 'd) t
-(**Main type. It stores two axes and a way to print it (see type [axes]).*)
+  (**Main type. It stores two axes and a way to print it (see type [axes]).*)
 
 val make_axis :
   ([> `P of Pointstyle.name] as 'a) -> 'a ->
   ([> `Linear | `Logarithmic ] as 'b) ->
   mode_tics ->
   ([> `Numbers | `Other of string list ] as 'c) -> ('a, 'b, 'c) axis
-(**Makes an axis, given a way to make tics, their positionment, their
-number and the text to apply near them.*)
+  (**Makes an axis, given a way to make tics, their positionment, their
+     number and the text to apply near them.*)
 
 val make :
- ([> `Rectangle of bool * bool | `Two_lines of float * float ] as 'a) ->
+  ([> `Rectangle of bool * bool | `Two_lines of float * float ] as 'a) ->
   ('b, 'c, 'd) axis -> ('b, 'c, 'd) axis -> ('a, 'b, 'c, 'd) t
+  (**Given axes and how to stroke them, makes a [t].*)
+
+val print_axes :
+  [< `Rectangle of bool * bool | `Two_lines of float * float ] ->
+  xmin:float ->
+  xmax:float -> ymin:float -> ymax:float -> Coord_handler.t -> float * float
+  (**Function used to print axes. It returns the point where the axes meet.*)
+
 
 val print_tic : Coord_handler.t -> [< `P of Pointstyle.name ] -> unit
+  (**This function is used to tell how to print a tic.*)
 
 val get_funct :
   [< `Linear | `Logarithmic ] -> int -> int -> int -> float * bool
+  (**[get_funct loc] returns a function whose purpose is to tell where
+     a tic is needed and if it is a major one. More precisely, the
+     arguments of this function are, in order, [majors minors num], where
+     [majors] is the number of major tics (including the first and the
+     last one), [minors] the number of minor tics between two major tics,
+     and [num] the number of the tic considered. The function then
+     returns a number between [0.] and [1.] which gives the relative
+     position of the tic, and if it is a major one.*)
 
-val get_text :
-  [< `Linear | `Logarithmic ] ->
-  [< `Numbers | `Other of string list ] ->
-  float -> float -> int -> int -> string
+val get_labels :
+  [< `Numbers | `Other of string list ] -> unit -> string
+  (**Returns an iterator to make labels on an axis.*)
 
 val print :
-  ([< `Rectangle of bool * bool | `Two_lines of float * float ],
-   [< `P of Pointstyle.name],
-   [< `Linear | `Logarithmic ],
-   [< `Numbers | `Other of string list ])
-  t ->
-  xmin:float -> xmax:float -> ymin:float -> ymax:float -> Coord_handler.t -> unit
+  ([< `Rectangle of bool * bool | `Two_lines of float * float ] as 'a,
+   [< `P of Pointstyle.name] as 'b,
+   [< `Linear | `Logarithmic ] as 'c,
+   [< `Numbers | `Other of string list ] as 'd) t ->
+  xmin:float -> xmax:float -> ymin:float -> ymax:float ->
+  ?print_tic:(Coord_handler.t -> 'b -> unit) ->
+  ?get_funct:('c -> int -> int -> int -> float * bool) ->
+  ?get_labels:('d -> unit -> string) ->
+  Coord_handler.t -> unit
