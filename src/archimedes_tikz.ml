4ad353d4643d88f459b39c91f0ed76aac084b8fe
Bertrand Desmons <bibi-6@hotmail.fr>
2009-08-14 12:56:18 +0200

    TikZ backend.

---
+++ src/archimedes_tikz.ml
@@ -0,0 +1,474 @@
+(* File: archimedes_tikz.ml
+
+   Copyright (C) 2009
+
+     Bertrand Desmons <Bertrand.Desmons@umons.ac.be>
+     Christophe Troestler <Christophe.Troestler@umons.ac.be>
+     WWW: http://math.umh.ac.be/an/software/
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License version 3 or
+   later as published by the Free Software Foundation, with the special
+   exception on linking described in the file LICENSE.
+
+   This library is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
+   LICENSE for more details. *)
+
+(** Ti{i k}Z Archimedes plugin *)
+
+open Printf
+open Archimedes
+module B = Backend
+
+(* Re-export the labels so we do not have to qualify them with [Backend]. *)
+type matrix = Backend.matrix = { mutable xx: float; mutable yx: float;
+                                 mutable xy: float; mutable yy: float;
+                                 mutable x0: float; mutable y0: float; }
+
+let is_infinite x = 1. /. x = 0.
+let min a b = if (a:float) < b then a else b
+let max a b = if (a:float) > b then a else b
+
+let round x =
+  truncate(if x >= 0. then x +. 0.5 else x -. 0.5)
+
+(** Return the smaller rectangle including the rectangle [r] and the
+    segment joining [(x0,y0)] and [(x1,y1)]. *)
+let update_rectangle r x0 y0 x1 y1 =
+  let x = min r.Backend.x (min x0 x1)
+  and y = min r.Backend.y (min y0 y1)
+  and x' = max (r.Backend.x +. r.Backend.w) (max x0 x1)
+  and y' = max (r.Backend.y +. r.Backend.h) (max y0 y1) in
+  { Backend.x = x;  y = y;  w = x' -. x;  h = y' -. y }
+
+(** Returns the range of the function f = t -> (1-t)**3 x0 + 3
+    (1-t)**2 t x1 + 3 (1-t) t**2 x2 + t**3 x3, 0 <= t <= 1, under the
+    form of an interval [xmin, xmax] *)
+let range_bezier x0 x1 x2 x3 =
+  let f t =
+    let t' = 1. -. t in
+    let t2 = t *. t in
+    t' *. (t' *. (t' *. x0 +. 3. *. t *. x1) +. 3. *. t2 *. x2)
+    +. t2 *. t *. x3 in
+  let a = x3 -. 3. *. x2 +. 3. *. x1 -. x0
+  and b = 2. *. x2 -. 4. *. x1 +. 2. *. x0
+  and c = x1 -. x0 in
+  if a = 0. then
+    if b = 0. then min x0 x3, max x0 x3 (* deg 1 (=> monotone) *)
+    else
+      let root = -. c /. b in
+      if 0. < root && root < 1. then
+        let x = f root in min x (min x0 x3), max x (max x0 x3)
+      else min x0 x3, max x0 x3         (* monotone for t in [0,1] *)
+  else
+    let delta = b *. b -. 4. *. a *. c in
+    if delta < 0. then min x0 x3, max x0 x3 (* monotone *)
+    else if delta = 0. then
+      let root = -. b /. (2. *. a) in
+      if 0. < root && root < 1. then
+        let x = f root in min x (min x0 x3), max x (max x0 x3)
+      else min x0 x3, max x0 x3         (* monotone for t in [0,1] *)
+    else (* delta > 0. *)
+      let root1 = (if b >= 0. then -. b -. sqrt delta
+                   else -. b +. sqrt delta) /. (2. *. a) in
+      let root2 = c /. (a *. root1) in
+      if 0. < root1 && root1 < 1. then
+        let f1 = f root1 in
+        if 0. < root2 && root2 < 1. then
+          let f2 = f root2 in
+          min (min f1 f2) (min x0 x3), max (max f1 f2) (max x0 x3)
+        else min f1 (min x0 x3), max f1 (max x0 x3)
+      else (* root1 outside [0,1] *)
+        if 0. < root2 && root2 < 1. then
+          let f2 = f root2 in
+          min f2 (min x0 x3), max f2 (max x0 x3)
+        else min x0 x3, max x0 x3
+;;
+
+(** Return the smaller reactangle containing [r] and the Bézier curve
+    given by the control points. *)
+let update_curve r x0 y0 x1 y1 x2 y2 x3 y3 =
+  let xmin, xmax = range_bezier x0 x1 x2 x3 in
+  let xmin = min xmin r.Backend.x in
+  let w = max xmax (r.Backend.x +. r.Backend.w) -. xmin in
+  let ymin, ymax = range_bezier y0 y1 y2 y3 in
+  let ymin = min ymin r.Backend.y in
+  let h = max ymax (r.Backend.y +. r.Backend.h) -. ymin in
+  { Backend.x = xmin;  y = ymin; w = w; h = h }
+
+
+let point_string x y = "("^(string_of_float x)", "^(string_of_float y)")"
+
+module B =
+struct
+  let name = "tikz"
+
+   (* Record the state of various graphics values*)
+  type state = {
+    mutable color: string;
+    mutable opacity: string;
+    mutable linewidth:string;
+    mutable dash_offset: string;
+    mutable dash: string;
+    mutable dash_data: float array * float;
+    mutable line_cap: string;
+    mutable line_join: string;
+    mutable miter_limit: string;
+    (*All options, ready to be printed.*)
+    mutable curr_pt: bool;
+    (*Says whether a current point is active.*)
+    mutable x:float;
+    mutable y:float;
+    (*Coordinates of the current point (if any)*)
+    mutable path_extents: Backend.rectangle;
+    (* The extent of the current path, in device coordinates. *)
+    mutable ctm : matrix; (* current transformation matrix from the
+                             user coordinates to the device ones. *)
+  }
+
+  type t = {
+    mutable color_number: int (*used to give colors a unique name*)
+    mutable closed: bool;
+    mutable state:state;
+    history: state Stack.t;
+    writer: out_channel;(*writes on the file*)
+    mutable indent:int;(*indentation of TikZ code*)
+   }
+
+  let write t txt =
+    let spaces = String.make t.indent ' ' in
+    output_string t.writer (spaces^txt^"\r\n")
+
+  let check_valid_handle t =
+    if t.closed then failwith "Archimedes_tikz: handle closed"
+
+  let get_state t =
+    check_valid_handle t; t.state
+
+ (* let print_ctm st =
+    let m = st.ctm in
+    Printf.sprintf "{ %f, %f, %f, %f, (%f, %f)}" m.xx m.xy m.yx m.yy m.x0 m.y0*)
+
+  let make_options t =
+   (* let st = get_state t in
+    "cm="^print_ctm st^","^*)
+      st.color^", "^
+      st.opacity^", "^
+      st.linewidth^", "^
+      st.dash_offset^", "^
+      st.dash^", "^
+      st.line_cap^", "^
+      st.line_join^", "^
+      st.miter_limit
+
+  let save t =
+    let st = get_state t in
+    (* Make a copy of the record so that further actions do not modify it *)
+    let state_copy = { st with color = st.color } in
+    Stack.push state_copy t.history;
+    write t "\begin{scope}";
+    t.indent <- t.indent + 2;
+
+  let restore t =
+    check_valid_handle t;
+    try
+      let st = Stack.pop t.history in
+      t.state <- st;
+      (* Re-enable previous settings in case they were changed *)
+      write t "\end{scope}";
+      t.indent <- t.indent - 2;
+    with Stack.Empty -> ()
+
+  let make ~options width height =
+    match options with
+      [name] ->
+        let writer = open_out name in
+        output_string writer "%%%Generated by Archimedes\r\n";
+        output_string writer "\begin{tikzpicture}[x=1pt,y=1pt]\n";
+        (*FIXME: coherence with other backends.*)
+        let w,h = string_of_float w, string_of_float h in
+        let device_rectangle =
+          "(0,0) rectangle ("^w","^h^")"
+        in
+        (*Forbids automatic scaling of TikZ (we do it in higher levels)*)
+        output_string writer
+          ("\clip[use as bounding box] "^device_rectangle^"\n");
+        let state = {
+          color = "color=black";
+          opacity = "opacity=opaque";
+          linewidth ="line width=1";
+          dash_offset = "dash phase=0";
+          dash = "dash pattern=plain";
+          dash_data = [||], 0.;
+          line_cap = "line cap=butt";
+          line_join = "line join=miter";
+          miter_limit = "miter limit=10";
+          curr_pt = false; x=0.;y=0.; (*No current point*)
+          path_extents = { Backend.x=0.; y=0.; w=0.; h=0. };
+          (* Identity transformation matrix *)
+          ctm = Matrix.make_identity();
+        } in
+        { closed = false;
+          history = Stack.create();
+          state = state;
+          curr_path = "";
+          writer = writer;
+          indent = 2;
+        }
+    | _ -> invalid_arg "Archimedes_tikz.make"
+
+  let close ~options t =
+    if not(t.closed) then (
+      write t "\end{tikzpicture}";
+      close_out t.writer;
+      t.closed <- true;
+    )
+
+  let set_color t c =
+    let st =  get_state t in
+    let r,g,b,a = Color.get_rgba c in
+    write t (Printf.sprintf "\definecolor{archimedes_tikz %i}{%f, %f, %f}"
+               st.color_number r g b);
+    st.color <- "color=archimedes_tikz "^(string_of_int st.color_number);
+    st.opacity <- "opacity="^(string_of_float a);
+    st.color_number <- st.color_number + 1;
+
+  let set_line_width t w =
+    let st = get_state t in
+    st.line_width <- sprintf "line width=%f" w;
+
+  let get_line_width t =
+    let s = (get_state t).line_width in
+    let len = String.length s in
+    let s' = String.sub 11 (len - 13) in (*removes 'line width=' and 'cm'*)
+    float_of_string s'
+
+
+  let set_dash t ofs arr =
+    let st = get_state t in
+    st.dash_offset <- "dash phase="^(string_of_float ofs);
+    let switch = function
+      | "on " -> "off "
+      | "off " -> "on "
+      | _ -> "" (*won't be used*)
+    in
+    let make_dash i mode str =
+      try
+        let data = string_of_float arr.(i) in
+        let new_str = str^mode^data^" " in
+        make_dash (i+1) (switch mode) new_str
+      with Invalid_argument ioob ->
+        if str = "" then "plain"
+        else if i mod 2 <> 0 then (*adds an "off" with last available value*)
+          str^mode^(string_of_float arr.(i-1))^" "
+        else str
+    in
+    st.dash <- "dash pattern="^(make_dash 0 "on " "");
+    st.dash_data <- arr, ofs
+
+  let get_dash t =
+    let st = get_state t in st.dash_data
+
+  let set_line_cap t cap =
+    let st = get_state t in
+    st.line_cap <- "line cap="^
+      (match cap with
+         B.BUTT -> "butt"
+       | B.SQUARE -> "rect"
+       | B.ROUND -> "round")
+
+  let get_line_cap t =
+    let st = get_state t in
+    match String.sub st.line_cap 9 (String.length st.line_cap - 9) with
+      (*Removes first characters: 'line cap='*)
+      "butt" -> B.BUTT
+    | "rect" -> B.SQUARE
+    | "round" -> B.ROUND
+    | _ -> failwith ("Archimedes TikZ.line_cap: error; cannot parse "
+                     ^( String.sub st.line_cap 9 (String.length st.line_cap - 9)))
+
+
+  let set_line_join t join =
+    let st = get_state t in
+    st.line_join <- "line join="^
+      (match join with
+         B.JOIN_BEVEL -> "bevel"
+       | B.JOIN_MITER -> "miter"
+       | B.JOIN_ROUND -> "round")
+
+  let get_line_join t =
+    let st = get_state t in
+    match String.sub st.line_join 10 (String.length st.line_join - 10) with
+      (*Removes first characters: 'line join='*)
+      "bevel" -> B.JOIN_BEVEL
+    | "miter" -> B.JOIN_MITER
+    | "round" -> B.JOIN_ROUND
+    | _ -> failwith ("Archimedes TikZ.line_join: error; cannot parse "
+                     ^( String.sub st.line_join 10
+                          (String.length st.line_join - 10)))
+
+
+  let set_miter_limit t lim =
+    let st = get_state t in
+    st.miter_limit <- "miter limit="(string_of_float lim)
+
+  (* Paths are not acted upon directly but wait for [stroke] or [fill]. *)
+  let move_to t ~x ~y =
+    let st = get_state t in
+    let x,y = B.Matrix.transform_point x y in
+    (* move only updates the current point but not the path extents *)
+    st.curr_pt <- true;
+    st.x <- x;
+    st.y <- y;
+    t.curr_path <- t.curr_path^" "^(point_string x y)
+
+  let line_to t ~x ~y =
+    let st = get_state t in
+    let x',y' = B.Matrix.transform_point x y in
+    (*Note: if there's no current point then line_to behaves as move_to.*)
+    if st.curr_pt then (
+      t.curr_path <- t.curr_path^" -- "^(point_string x' y')
+      (* Update extents and current point *)
+      let x0', y0' = Matrix.transform_point st.ctm st.x st.y in
+      st.path_extents <- update_rectangle st.path_extents x0' y0' x' y';
+    )
+    else
+    t.curr_path <- t.curr_path^" "^(point_string x' y');
+    st.curr_pt <- true;
+    st.x <- x';
+    st.y <- y'
+
+  let rel_move_to t ~x ~y =
+    let st = get_state t in
+    let x,y = B.Matrix.transform_point x y in
+    t.curr_path <- t.curr_path^" ++"^(point_string x y)
+    st.curr_pt <- true;
+    st.x <- st.x +. x;
+    st.y <- st.y +. y
+
+  let rel_line_to t ~x ~y =
+    let st = get_state t in
+    let x,y = B.Matrix.transform_point x y in
+    t.curr_path <- t.curr_path^" -- ++"^(point_string x y)
+    st.curr_pt <- true;
+    st.x <- st.x +. x;
+    st.y <- st.y +. y
+
+
+  let curve_to t ~x1 ~y1 ~x2 ~y2 ~x3 ~y3 =
+    (* Suffices to transform the control point by the affine
+       transformation to have the affine image of the curve *)
+    let st = get_state t in
+    let x1', y1' = Matrix.transform_point st.ctm x1 y1 in
+    let x2', y2' = Matrix.transform_point st.ctm x2 y2 in
+    let x3', y3' = Matrix.transform_point st.ctm x3 y3 in
+    if not st.curr_pt then t.curr_path <- t.curr_path^" "^(point x1' y1');
+    t.curr_path <- t.curr_path^" ..controls "^(point x1' y1')^
+      " and "^(point x2' y2')^".. "^(point x3' y3');
+    (* Update the current point and extents *)
+    st.path_extents <-
+      update_curve st.path_extents x0' y0' x1' y1' x2' y2' x3' y3';
+    st.curr_pt <- true;
+    st.x <- x3';
+    st.y <- y3'
+
+
+  let rectangle t ~x ~y ~w ~h =
+    let st = get_state t in
+    let x', y' = Matrix.transform_point st.ctm x y
+    and w', h' = Matrix.transform_distance st.ctm w h in
+    t.curr_path <-t.curr_path^" "^(point x y)^" rectangle "^
+      (point (x'+.w') (y'+.h'));
+    (* Update the current point and extents *)
+    st.path_extents <-
+      update_rectangle st.path_extents x' y' (x' +. w') (y' +. h');
+    st.curr_pt <- true;
+
+
+  let arc t ~x ~y ~r ~a1 ~a2 =
+    ()
+
+  let close_path t =
+    let st = get_state t in
+    if st.curr_pt then t.curr_path <- t.curr_path^" -- cycle"
+
+  let clear_path t =
+    check_valid_handle t;
+    t.curr_path <- "";
+    t.state.path_extents <- { Backend.x=0.; y=0.; w=0.; h=0. }
+
+  let path_extents t = (get_state t).path_extents
+
+  let stroke_preserve t =
+    let opts = make_options t in
+    write t ("\draw["^opts^"] ");
+    t.indent <- t.indent + 2;
+    write t (t.curr_path^";");
+    t.indent <- t.indent - 2
+
+
+  let stroke t = stroke_preserve t; clear_path t
+
+  let fill_preserve t =
+    write t ("\fill["^opts^"] ");
+    t.indent <- t.indent + 2;
+    write t (t.curr_path^";");
+    t.indent <- t.indent - 2
+
+  let fill t = fill_preserve t; clear_path t
+
+  let clip_rectangle t ~x ~y ~w ~h =
+    write t ("\clip "^(point x y)^" rectangle "^(point (x+.w) (y+.h)";")
+
+
+  let translate t ~x ~y = Matrix.translate (get_state t).ctm x y
+
+  let scale t ~x ~y = Matrix.scale (get_state t).ctm x y
+
+  let rotate t ~angle = Matrix.rotate (get_state t).ctm ~angle
+
+  let set_matrix t m = (get_state t).ctm <- m
+
+  let get_matrix t = Matrix.copy (get_state t).ctm
+
+  let select_font_face t slant weight family =
+    ()
+
+  let set_font_size t size =
+    ()
+
+  let text_extents t txt =
+    let w, h = Graphics.text_size txt in
+    { Backend.x = 0.; y = 0.; w = float w; h = float h }
+
+  let show_text t ~rotate ~x ~y pos txt =
+    let st = get_state t in
+    (* Compute the angle between the desired direction and the X axis
+       in the device coord. system. *)
+    let dx, dy = Matrix.transform_distance st.ctm (cos rotate) (sin rotate) in
+    let angle = atan2 dy dx in
+    let x', y' = Matrix.transform_point st.ctm x y in
+    let pos = match pos with
+      | B.LT -> "south east"
+      | B.LC -> "east"
+      | B.LB -> "north east"
+      | B.CT -> "south"
+      | B.CC -> "center"
+      | B.CB -> "north"
+      | B.RT -> "south west"
+      | B.RC -> "west"
+      | B.RB -> "north west"
+    in
+    write t (Printf.sprintf "\node [anchor=%s,rotate=%f] at %s {%s}"
+               pos angle (point x' y') txt
+end
+
+let () =
+  let module U = Backend.Register(B)  in ()
+
+
+(* Local Variables: *)
+(* compile-command: "make -k archimedes_graphics.cmo" *)
+(* End: *)
