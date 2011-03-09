f42bc2751dd8750fca1f6fc565f9a89bfd73e595
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2011-03-09 10:12:48 +0100

    Rearrangments of the elements for the "line" test.

--- tests/backend_line.ml
+++ tests/backend_line.ml
@@ -4,7 +4,7 @@ open Archimedes
 let pi = 4. *. atan 1.
 
 let draw bk =
-  let bk = Backend.make bk 800. 700. ~dirs:[ "./src"; "../src" ] in
+  let bk = Backend.make bk 700. 700. ~dirs:[ "./src"; "../src" ] in
 
   (* Only the size at the time of stroking is important. *)
   Backend.set_line_width bk 1.;
@@ -13,6 +13,7 @@ let draw bk =
   Backend.set_line_width bk 100.;
   Backend.stroke bk;
 
+  (* Arc *)
   Backend.set_line_width bk 70.;
   Backend.move_to bk 100. 450.;
   Backend.arc bk 200. pi (2.5 *. pi);
@@ -23,14 +24,14 @@ let draw bk =
   Backend.set_line_width bk 10.;
   Backend.stroke bk;
 
-  (* The line width is relative to the current CTM *)
+  (* The line width and coordinates are relative to the current CTM *)
   Backend.set_line_width bk 10.;
-  Backend.move_to bk 400. 50.;
-  Backend.line_to bk 500. 50.;
+  Backend.move_to bk 200. 500.;
+  Backend.line_to bk 350. 500.;
   Backend.stroke bk;
   Backend.scale bk 1. 4.;
-  Backend.move_to bk 400. 20.;
-  Backend.line_to bk 500. 20.;
+  Backend.move_to bk 200. 110.;
+  Backend.line_to bk 350. 110.;
   Backend.stroke bk;
 
   Backend.close bk
