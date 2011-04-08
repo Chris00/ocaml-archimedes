a290f1bbbee458dd65dc1b820ee3ab5dd8976db2
Noémie Meunier <noemie_6462@hotmail.com>
2011-04-08 15:25:38 +0200

    Sketch interface of viewport

--- src/viewport.ml
+++ src/viewport.ml
@@ -4,7 +4,7 @@
 
      Christophe Troestler <Christophe.Troestler@umons.ac.be>
      Pierre Hauweele <antegallya@gmail.com>
-     Noémie Meunier <noemie_6462@hotmail.com>
+     Noemie Meunier <noemie_6462@hotmail.com>
      Fabian Pijcke <fabian.pijcke@gmail.com>
      WWW: http://math.umons.ac.be/an/software/
 
@@ -18,8 +18,7 @@
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
    LICENSE for more details. *)
 
-module rec Sizes =
-struct
+module rec Sizes : sig
   type size =
     | Absolute of float
     | Rel_not_updated of float
@@ -28,7 +27,27 @@ struct
   type t = {
     parent: t;
     children: t list;
-    
+
+    mutable line_width: size;
+    mutable text_size: size;
+    mutable mark_size: size
+  }
+
+  val make_root : size -> size -> size -> t
+  val make : t -> size -> size -> size -> t
+  val make_rel : t -> float -> float -> float -> t
+  val make_abs : t -> float -> float -> float -> t
+end
+= struct
+  type size =
+    | Absolute of float
+    | Rel_not_updated of float
+    | Rel_updated of float * float
+
+  type t = {
+    parent: t;
+    children: t list;
+
     mutable line_width: size;
     mutable text_size: size;
     mutable mark_size: size
@@ -37,21 +56,28 @@ struct
   let make_root lw ts ms =
     let rec root =
       { parent = root;
-	children = [];
-	line_width = lw; text_size = ts; mark_size = ms }
+        children = [];
+        line_width = lw; text_size = ts; mark_size = ms }
     in
-      root
+    root
 
   let make parent lw ts ms =
-    let sizes = 
-      { parent = parent; 
-	children = [];
-	line_width = lw; text_size = ts; mark_size = ms } in
-      parent.children <- sizes :: parent.children;
-      sizes
+    let sizes =
+      { parent = parent;
+        children = [];
+        line_width = lw;
+        text_size = ts;
+        mark_size = ms } in
+    parent.children <- sizes :: parent.children;
+    sizes
+
+  let make_rel parent lw ts ms =
+    make parent (Rel_not_updated lw) (Rel_not_updated ts) (Rel_not_updated ms)
+
+  let make_abs parent lw ts ms =
+    make parent (Absolute lw) (Absolute ts) (Absolute ms)
 end
-and Axes =
-struct
+and Axes : sig
   type labels =
     | Text of string array * float
     | Number
@@ -65,7 +91,7 @@ struct
     | Equidistants of int * int
     | Auto
 
-  type sign = Positive | Negative;
+  type sign = Positive | Negative
 
   type offset =
     | Relative of float
@@ -91,8 +117,84 @@ struct
 
     mutable viewports: Viewport.t list
   }
+
+  val default_axis: unit -> axis
+  val default_axes_system: Viewport.t list -> t
 end
-and Viewport = struct
+= struct
+  type labels =
+    | Text of string array * float
+    | Number
+    | Expnumber of float
+    | Expnumber_named of float * string
+    | Custom of (float -> string)
+
+  type tics =
+    | Fixed of float list
+    | Fixed_norm of float list
+    | Equidistants of int * int
+    | Auto
+
+  type sign = Positive | Negative
+
+  type offset =
+    | Relative of float
+    | Absolute of float
+
+  type graph_axis = {
+    tics: tics;
+    offset: float;
+    tics_position: sign
+  }
+
+  type axis = {
+    mutable x0: float;     mutable auto_x0: bool;
+    mutable xend: float;   mutable auto_xend: bool;
+    mutable log: bool;
+    mutable orientation: sign;
+    mutable graph_axes: graph_axis list
+  }
+
+  type t = {
+    x: axis;
+    y: axis;
+
+    mutable viewports: Viewport.t list
+  }
+
+  let default_axis () =
+    { x0 = -1.; xend = 1.; auto_x0 = true; auto_xend = true;
+      log = false; orientation = Positive; graph_axes = [] }
+
+  let default_axes_system viewports =
+    { x = default_axis ();
+      y = default_axis ();
+      viewports = viewports }
+end
+and Viewport : sig
+  type t = {
+    backend: Backend.t;
+    mutable coord_device: Coordinate.t; (* AA *)
+    mutable coord_graph: Coordinate.t; (* BB *)
+    mutable coord_orthonormal: Coordinate.t; (* AE *)
+    mutable coord_data: Coordinate.t; (* CC *)
+
+    mutable axes_system: Axes.t;
+    mutable sizes: Sizes.t;
+    mutable current_point: float * float;
+    mutable instructions: (unit -> unit) Queue.t;
+    mutable immediate_drawing: bool
+  }
+
+  type coord_name = Device | Graph | Data | Orthonormal
+
+  val get_coord_from_name : viewport -> coord_name -> Coordinate.t
+  val init : ?lines:float -> ?text:float -> ?marks:float -> ?w:int -> ?h:int ->
+    dirs:string list -> string -> viewport
+  val make : ?lines:float -> ?text:float -> ?marks:float -> viewport ->
+    coord_name -> float -> float -> float -> float -> viewport
+end
+= struct
   type t = {
     backend: Backend.t;
 
@@ -122,76 +224,68 @@ and Viewport = struct
     mutable immediate_drawing: bool
   }
 
-  type Coord_name = Device | Graph | Data | Orthonormal
+  type coord_name = Device | Graph | Data | Orthonormal
 
   let init ?(lines=0.002) ?(text=0.024) ?(marks=0.01) ?(w=640) ?(h=480) ~dirs backend_name =
     let backend = Backend.make ~dirs backend_name w h in
     let coord_root = Coordinate.make_root (Backend.get_matrix backend) in
     let size0 = min w h in
-    let axis = {
-      Axis.x0 = -1.; Axis.auto_x0 = true;
-      Axis.xend = 1.; Axis.auto_xend = true;
-      Axis.log = false;
-      Axis.orientation = Axis.Positive;
-      Axis.graph_axes = []
-    } in
-    let rec axes_system = {
-      x = axis;
-      y = {axis with Axis.x0 = axis.Axis.x0};
-      viewports = [viewport]
-    }
+    let coord_device = Coordinate.make_identity coord_root in
+    let coord_graph = Coordinate.make_scale (Coordinate.make_translate coord_device 0.1 0.1) 0.8 0.8 in
+    let rec axes_system = Axes.default_axes_system [viewport]
     and viewport = {
       backend = backend;
-      coord_device = Coordinate.make_identity coord_root;
-      coord_graph = Coordinate.make_scale (Coordinate.make_translate coord_root 0.1 0.1) 0.8 0.8;
-      coord_orthonormal = Coordinate.make_scale coord_root (size0 /. w) (size0 /. h);
+      coord_device = coord_device; coord_graph = coord_graph;
+      coord_orthonormal = Coordinate.make_scale coord_device (size0 /. w) (size0 /. h);
       (* We don't care; will be updated as soon as points are added or we change axes. *)
-      coord_data = Coordinate.make_identity coord_root;
+      coord_data = Coordinate.make_identity coord_graph;
       axes_system = axes_system;
       sizes = Sizes.make (Sizes.make_root size0 size0 1.) lines text marks;
       current_point = (0., 0.);
       instructions = Queue.create ();
       immediate_drawing = false;
     } in
-      viewport
+    viewport
 
-  (* TODO *)
+  let get_coord_from_name vp = function
+    | Device -> vp.coord_device
+    | Graph -> vp.coord_graph
+    | Data -> vp.coord_data
+    | Orthonormal -> vp.coord_orthonormal
+
   let make ?(lines=0.002) ?(text=0.024) ?(marks=0.01) vp coord_name xmin xmax ymin ymax =
-    let size0 =
-      let xmax', ymax' = Coordinate.to_device coord xmax ymax in
-      let xmin', ymin' = Coordinate.to_device coord xmin ymin in
-	min (xmax' -. xmin') (ymax' -. ymin')
+    let w, h, size0 =
+      let xmax', ymax' = Coordinate.to_device coord xmax ymax
+      and xmin', ymin' = Coordinate.to_device coord xmin ymin in
+      let w = xmax' -. xmin' and h = ymax' -. ymin' in
+      w, h, min w h
     in
-    let axis = {
-      Axis.x0 = -1.; Axis.auto_x0 = true;
-      Axis.xend = 1.; Axis.auto_xend = true;
-      Axis.log = false;
-      Axis.orientation = Axis.Positive;
-      Axis.graph_axes = []
-    } in
-    let rec axes_system = {
-      x = axis;
-      y = {axis with Axis.x0 = axis.Axis.x0};
-      viewports = [viewport]
-    }
-    and viewport = {
-      backend = backend;
-      coord_device = coord_root;
-      coord_graph = coord_root;
-      coord_orthonormal = coord_root;
-      coord_data = coord_root;
-      axes_system = axes_system;
-      sizes = {
-        Sizes.line_width = Sizes.Absolute size0;
-        Sizes.text_size = Sizes.Absolute size0;
-        Sizes.mark_size = Sizes.Absolute 1. };
+    let coord_parent = get_coord_from_name vp coord_name in
+    let coord_device = Coordinate.make_translation
+      (Coordinate.make_scale coord_parent (xmax -. xmin) (ymax -. ymin)) xmin ymin in
+    let coord_graph = Coordinate.make_scale (Coordinate.make_translate coord_device 0.1 0.1) 0.8 0.8 in
+    let rec viewport = {
+      backend = vp.backend;
+      coord_device = coord_device; coord_graph = coord_graph;
+      coord_orthonormal = Coordinate.make_scale coord_device (size0 /. w) (size0 /. h);
+      (* We don't care; will be updated as soon as points are added or we change axes. *)
+      coord_data = Coordinate.make_identity coord_graph;
+      axes_system = Axes.default_axes_system [viewport];
+      sizes = Sizes.make_rel vp.sizes lines text marks;
       current_point = (0., 0.);
       instructions = [];
       immediate_drawing = false;
     } in
-    let root_sizes = {
-      Sizes.line_width = Sizes.Rel_updated (lines, lines *. size0);
-      Sizes.text_size = Sizes.Rel_updated (text, text *. size0);
-      Sizes.mark_size = Sizes.Rel_updated (marks, marks) } in
-      {real_root with sizes = root_sizes; parent = real_root}
+    viewport
+
+  let do_instructions vp = ()
+
+  let close vp =
+    let parent = vp.coord_device.Coordinate.parent in
+    parent.children <- List.filter (( <> ) vp) parent.children
+    (* TODO do we need to do instructions ? *)
+    if parent == parent.Coordinate.parent then begin
+      do_instructions vp;
+      Backend.close vp.backend
+    end
 end
--- src/viewport.mli
+++ src/viewport.mli
@@ -1,10 +1,10 @@
-(* File: axes.mli
+(* File: viewport.mli
 
-   Copyright (C) 2009
+   Copyright (C) 2009-2015
 
      Christophe Troestler <Christophe.Troestler@umons.ac.be>
      Pierre Hauweele <antegallya@gmail.com>
-     Noémie Meunier <noemie_6462@hotmail.com>
+     Noemie Meunier <noemie_6462@hotmail.com>
      Fabian Pijcke <fabian.pijcke@gmail.com>
      WWW: http://math.umons.ac.be/an/software/
 
@@ -18,4 +18,165 @@
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
    LICENSE for more details. *)
 
-include Axes_public.T
+module rec Sizes : sig
+  type size =
+    | Absolute of float
+    | Rel_not_updated of float
+    | Rel_updated of float * float
+
+  type t = {
+    parent: t;
+    children: t list;
+
+    mutable line_width: size;
+    mutable text_size: size;
+    mutable mark_size: size
+  }
+
+  val make_root : size -> size -> size -> t
+  val make : t -> size -> size -> size -> t
+  val make_rel : t -> float -> float -> float -> t
+  val make_abs : t -> float -> float -> float -> t
+end
+and Axes : sig
+  type labels =
+    | Text of string array * float
+    | Number
+    | Expnumber of float
+    | Expnumber_named of float * string
+    | Custom of (float -> string)
+
+  type tics =
+    | Fixed of float list
+    | Fixed_norm of float list
+    | Equidistants of int * int
+    | Auto
+
+  type sign = Positive | Negative
+
+  type offset =
+    | Relative of float
+    | Absolute of float
+
+  type graph_axis = {
+    tics: tics;
+    offset: float;
+    tics_position: sign
+  }
+
+  type axis = {
+    mutable x0: float;     mutable auto_x0: bool;
+    mutable xend: float;   mutable auto_xend: bool;
+    mutable log: bool;
+    mutable orientation: sign;
+    mutable graph_axes: graph_axis list
+  }
+
+  type t = {
+    x: axis;
+    y: axis;
+
+    mutable viewports: Viewport.t list
+  }
+
+  val default_axis: unit -> axis
+  val default_axes_system: Viewport.t list -> t
+end
+and Viewport : sig
+  type t = {
+    backend: Backend.t;
+    mutable coord_device: Coordinate.t;
+    mutable coord_graph: Coordinate.t;
+    mutable coord_orthonormal: Coordinate.t;
+    mutable coord_data: Coordinate.t;
+
+    mutable axes_system: Axes.t;
+    mutable sizes: Sizes.t;
+    mutable current_point: float * float;
+    mutable instructions: (unit -> unit) Queue.t;
+    mutable immediate_drawing: bool
+  }
+
+  type coord_name = Device | Graph | Data | Orthonormal
+
+  val get_coord_from_name : viewport -> coord_name -> Coordinate.t
+  val init : ?lines:float -> ?text:float -> ?marks:float -> ?w:int -> ?h:int ->
+    dirs:string list -> string -> viewport
+  val make : ?lines:float -> ?text:float -> ?marks:float -> viewport ->
+    coord_name -> float -> float -> float -> float -> viewport
+
+  val rows : t -> int -> viewport array
+  val columns : t -> int -> viewport array
+  val grid : t -> int -> int -> viewport array array
+
+  val set_line_width : t -> float -> unit
+  val set_mark_size : t -> float -> unit
+  val set_font_size : t -> float -> unit
+  val set_rel_line_width : t -> float -> unit
+  val set_rel_mark_size : t -> float -> unit
+  val set_rel_font_size : t -> float -> unit
+  val get_line_width : t -> float
+  val get_mark_size : t -> float
+  val get_font_size : t -> float
+
+  val width : t -> float
+  val height : t -> float
+  val set_color : t -> Color.t -> unit
+  val set_dash : t -> float -> float array -> unit
+  val set_line_join : t -> Backend.line_join -> unit
+  val get_line_width : t -> float
+  val get_line_cap : t -> Backend.line_cap
+  val get_dash : t -> float array * float
+  val get_line_join : t -> Backend.line_join
+  val move_to : t -> x:float -> y:float -> unit
+  val line_to : t -> x:float -> y:float -> unit
+  val rel_move_to : t -> x:float -> y:float -> unit
+  val rel_line_to : t -> x:float -> y:float -> unit
+  val curve_to :
+    t ->
+    x1:float ->
+    y1:float -> x2:float -> y2:float -> x3:float -> y3:float -> unit
+  val rectangle : t -> x:float -> y:float -> w:float -> h:float -> unit
+  val arc : t -> r:float -> a1:float -> a2:float -> unit
+  val close_path : t -> unit
+  val clear_path : t -> unit
+    (*val path_extents : t -> rectangle*)
+  val stroke_current : t -> unit
+  val stroke_current_preserve : t -> unit
+  val stroke : t -> unit
+  val stroke_preserve : t -> unit
+  val fill : t -> unit
+  val fill_preserve : t -> unit
+  val clip_rectangle : t -> x:float -> y:float -> w:float -> h:float -> unit
+  val save_vp : t -> unit
+  val restore_vp : t -> unit
+  val select_font_face : t -> Backend.slant -> Backend.weight -> string -> unit
+  val show_text :
+    t ->
+    rotate:float ->
+    x:float -> y:float -> Backend.text_position -> string -> unit
+    (*  val text_extents : t -> string -> rectangle*)
+  val render : t -> string -> unit
+    (* val mark_extents : t -> string -> rectangle *)
+
+  (* TODO Complete *)
+  val fx
+  val xy
+  val add_xaxis
+  val add_yaxis
+  val box_axes
+  val centered_axes
+
+  val xrange : t -> float -> float -> unit
+  val yrange : t -> float -> float -> unit
+
+  val xmin : t -> float
+  val xmax : t -> float
+  val ymin : t -> float
+  val ymax : t -> float
+
+
+  val close : viewport -> unit
+
+  val do_instructions : viewport -> unit
+end
