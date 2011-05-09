4d797795a9a77906318ee67d89ebe3ecdbef4794
Fabian Pijcke <fabian.pijcke@gmail.com>
2011-05-09 17:06:08 +0200

    dirty commit

--- src/plot.ml
+++ src/plot.ml
@@ -1,8 +1,11 @@
 (* File: plot.ml
 
-   Copyright (C) 2009
+   Copyright (C) 2009-2011
 
      Christophe Troestler <Christophe.Troestler@umons.ac.be>
+     Pierre Hauweele <antegallya@gmail.com>
+     Noemie Meunier <noemie_6462@hotmail.com>
+     Fabian Pijcke <fabian.pijcke@gmail.com>
      WWW: http://math.umons.ac.be/an/software/
 
    This library is free software; you can redistribute it and/or modify
@@ -15,36 +18,35 @@
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
    LICENSE for more details. *)
 
-open Bigarray
-module VP = Viewport
-
-type line_cap = Backend.line_cap = BUTT | ROUND | SQUARE
-
-type line_join = Backend.line_join = JOIN_MITER | JOIN_ROUND | JOIN_BEVEL
-
-type text_position = Backend.text_position =
- CC | LC | RC | CT | CB | LT | LB | RT | RB
-
-type slant = Backend.slant = Upright | Italic
-type weight = Backend.weight = Normal | Bold
+module V = Viewport
 
 module type COMMON = sig
-  val f : VP.t -> ?color:Color.t -> ?nsamples:int -> ?mark:string ->
-    ?fill:bool -> (float -> float) -> float -> float -> unit
+  type fill = In | Out
+  type filledcurves = Color.t * Color.t (* f1 > f2, f2 < f1 *)
 
-  val xyf : VP.t -> ?color:Color.t -> ?nsamples:int -> ?mark:string ->
-    ?fill:bool -> (float -> float * float) -> float -> float -> unit
+  val f : V.t -> ?nsamples:int -> ?fill:fill -> (float -> float) ->
+    float -> float -> unit
 
-  val text : VP.t -> ?rotate:float -> x:float -> y:float ->
-    ?pos:text_position -> string -> unit
+  val xy_param : V.t -> ?nsamples:int -> ?fill:fill ->
+    (float -> float * float) -> float -> float -> unit
+
+  (* TODO we want to have control over the stroke properties for each curve *)
+  val filledcurves : V.t -> ?nsamples:int -> ?fill:filledcurves ->
+    (float -> float) -> (float -> float) -> float -> float -> unit
 end
 
 module Common =
 struct
-  type viewport = Handle.viewport
 
-  let plot_f p ?color ?nsamples ?mark ?(fill=false) f a b fill0 fill1 =
-    draw_axes p;
+  let f vp ?(nsamples=100) ?fill f a b =
+    let logarithmic = vp.V.axes_system.Axes.x.Axes.log in
+    let step = if logarithmic then (b /. a) ** (1. /. nsamples)
+    else (b -. a) /. nsamples in
+
+
+
+
+  let plot_f vp ?nsamples ?(fill=false) f a b fill0 fill1 =
     let do_with, finish =
       if fill then
         let x_last = ref nan
@@ -53,41 +55,41 @@ struct
         ((fun p (x,y) ->
             if !first then (
               let x, y = fill0 x y in
-              Handle.move_to p x y;
+              V.move_to p x y;
               first := false
             )
             else
-              Handle.line_to p x y;
+              V.line_to p x y;
             x_last := x;
             y_last := y),
          (fun p ->
             let x, y = fill1 !x_last !y_last in
-            Handle.line_to p x y;
-            Handle.fill p;
+            V.line_to p x y;
+            V.fill p;
             first := true; (* this set of functions may be run several times *)
          ))
       else
         let first = ref true in
         ((fun p (x,y) ->
             if !first then (
-              Handle.move_to p x y;
+              V.move_to p x y;
               first := false
             )
             else
-              Handle.line_to p x y),
+              V.line_to p x y),
          (fun p ->
-            Handle.stroke p;
+            V.stroke p;
             first := true)) in
-    Handle.xyf p.h ?color ?nsamples ~do_with ~finish f a b;
+    V.xyf p.h ?color ?nsamples ~do_with ~finish f a b;
     (* Add marks if requested *)
     match mark with
     | None -> ()
     | Some mark ->
         let do_with p (x,y) =
-          Handle.move_to p x y;
-          Handle.render p mark
+          V.move_to p x y;
+          V.render p mark
         and finish _ = () in
-        Handle.xyf p.h ?color ?nsamples ~do_with ~finish f a b
+        V.xyf p.h ?color ?nsamples ~do_with ~finish f a b
   ;;
 
   let id x y = (x, y)
@@ -98,9 +100,9 @@ struct
   let f p ?color ?nsamples ?mark ?(fill=false) f a b =
     plot_f p ?color ?nsamples ?mark ~fill (fun x -> (x, f x)) a b pr_x pr_x
 
-  let set_font_size p w = Handle.set_global_font_size p.h w
+  let set_font_size p w = V.set_global_font_size p.h w
   let text p ?(rotate=0.) ~x ~y ?(pos=CC) txt =
-    Handle.show_text p.h ~rotate ~x ~y pos txt
+    V.show_text p.h ~rotate ~x ~y pos txt
 end
 
 (************************************************************************)
@@ -113,12 +115,12 @@ struct
     let n = Array.init (Array.length x) (fun i -> float(n0 + i)) in
     let iter = Iterator.of_arrays n x in
     (*draw_axes p;*)
-    Handle.xy p.h ?color ?mark iter
+    V.xy p.h ?color ?mark iter
 
   let xy p ?color ?mark x y =
     let iter = Iterator.of_arrays x y in
     (*draw_axes p;*)
-    Handle.xy p.h ?color ?mark iter
+    V.xy p.h ?color ?mark iter
 end
 
 module L = List
@@ -131,12 +133,12 @@ struct
     let n = List.map (fun _ -> incr i; float(!i)) x in
     let iter = Iterator.of_lists n x in
     (*draw_axes p;*)
-    Handle.xy p.h ?color ?mark iter
+    V.xy p.h ?color ?mark iter
 
   let xy p ?color ?mark x y =
     let iter = Iterator.of_lists x y in
     (*draw_axes p;*)
-    Handle.xy p.h ?color ?mark iter
+    V.xy p.h ?color ?mark iter
 end
 
 module Generic =
@@ -169,12 +171,12 @@ struct
     done;
     let iter = Iterator.of_bigarrays n x ~xclayout:false ~yclayout:false in
     (*draw_axes p;*)
-    Handle.xy p.h ?color ?mark iter
+    V.xy p.h ?color ?mark iter
 
   let xy p ?color ?mark x y =
     let iter = Iterator.of_bigarrays x y ~xclayout:false ~yclayout:false in
     (*draw_axes p;*)
-    Handle.xy p.h ?color ?mark iter
+    V.xy p.h ?color ?mark iter
 end
 
 module C =
@@ -190,11 +192,11 @@ struct
     done;
     let iter = Iterator.of_bigarrays n x ~xclayout:true ~yclayout:true in
     (*draw_axes p;*)
-    Handle.xy p.h ?color ?mark iter
+    V.xy p.h ?color ?mark iter
 
   let xy p ?color ?mark x y =
     let iter = Iterator.of_bigarrays x y ~xclayout:true ~yclayout:true in
     (*draw_axes p;*)
-    Handle.xy p.h ?color ?mark iter
+    V.xy p.h ?color ?mark iter
 
 end
