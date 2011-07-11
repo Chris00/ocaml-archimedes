27951f50eafd1110054b59b7292cf543ab96b03a
Fabian Pijcke <fabian.pijcke@gmail.com>
2011-07-11 09:56:29 +0200

    Missing file test_clip

---
+++ tests/test_clip.ml
@@ -0,0 +1,23 @@
+open Archimedes
+module V = Viewport
+
+let draw bk =
+  let f vp x y = Arrows.line vp x y (x -. y) (x +. y) in
+  let vp = V.init ~w:1024. ~h:600. ~dirs:["../src"; "./src"] bk in
+  let vp2 = V.make vp V.Device 0.3 0.7 0.2 0.8 (fun _ _ _ -> ()) in
+  V.xrange vp2 (-3.) 3.;
+  V.yrange vp2 (-2.) 2.;
+  for x = -5 to 5 do
+    for y = -5 to 5 do
+      f vp2 (float x +. 0.5) (float y +. 0.5)
+    done
+  done;
+  Axes.box vp2;
+  V.close vp
+
+let () =
+  List.iter draw [ "cairo PNG test_clip.png";
+                   (*"graphics hold";
+                   "tikz test_clip.tex"*)
+                 ]
+
