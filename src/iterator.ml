ff4344a8511b91cbc37341c090cc92c52cc90cb5
Fabian Pijcke <fabian.pijcke@gmail.com>
2011-07-11 17:00:30 +0200

    Sketch implementation of new iterators, non compiling revision

--- src/iterator.ml
+++ src/iterator.ml
@@ -18,208 +18,103 @@
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
    LICENSE for more details. *)
 
-module type Iterator = sig
-  type t
-  type data
+type ff = float * float
 
-  val of_data : data -> t
+type which =
+  | List of ff list * ff list
+  | Array of ff array
+  (* Le code des itérateurs sur fonctions n'est pas dans ce module mais
+     dans le module Sampler. Le but ici est simplement de fournir une
+     interface unifiée pour tous les types d'itérateurs. Par ailleurs,
+     l'itération sur deux fonctions en même temps reste à définir: Que
+     faire lorsque sur un sous-ensemble de R, la première fonction est
+     définie mais pas la seconde ? *)
+  | Function of Sampler.t
+  | C
+  | Fortran
 
-  val next : t -> (float * float) option
-  val reset : t -> unit
-  val iter : (float * float -> unit) -> t -> unit
-  val iter_cache : (float * float -> unit) -> t -> (float * float) list
-end
+type which2 =
+  | List2 of ff list * ff list * ff list * ff list
+  | Array2 of ff array * ff array
+  | Function2 of Sampler.t * Sampler.t
+  | C2
+  | Fortran2
 
-let rec iterate next f iter =
-  match next iter with
-  | None -> ()
-  | Some p -> f p; iterate next f iter
+exception EOI
 
-let rec iterate_cache next starting_list f iter =
-  match next iter with
-  | None -> starting_list
-  | Some p -> f p; iterate_cache next (p :: starting_list) f iter
+type t = {
+  mutable data : which;
+  mutable pos : int;
+}
 
-module List = struct
-  type data = float list
-  type t = {data:data; mutable data_curr:data; mutable pos:int}
+type t2 = {
+  mutable data2 : which2;
+  mutable pos2 : int;
+}
 
-  let of_data l = {
-    data = l;
-    data_curr = l;
-    pos = -1 (* that way, x starts from 0 *)
-  }
+let of_list l = {data = List (l, l); pos = 0}
+let of_array a = {data = Array a; pos = 0}
+let of_function f a b = {data = Function (Sampler.create f a b); pos = 0}
 
-  let reset iter =
-    iter.data_curr <- iter.data;
-    iter.pos <- -1
+let of_list2 l l' = {data2 = List2 (l, l, l', l'); pos2 = 0}
+let of_array2 a a' = {data2 = Array2 (a, a'); pos2 = 0}
+let of_function2 f f' a b = {data2 = Function2 (Sampler.create f a b,
+                                               Sampler.create f' a b);
+                             pos2 = 0}
 
-  let next iter = match iter.data_curr with
-    | [] -> None
-    | x :: tl ->
-        iter.data_curr <- tl;
-        iter.pos <- succ iter.pos;
-        Some (float iter.pos, x)
-
-  let iter = iterate next
-  let iter_cache = iterate_cache next []
-end
-
-module List2 = struct
-  type data = (float * float) list
-  type t = {data:data; mutable data_curr:data}
-
-  let of_data l = {
-    data = l;
-    data_curr = l
-  }
-
-  let reset iter =
-    iter.data_curr <- iter.data
-
-  let next iter = match iter.data_curr with
-    | [] -> None
-    | p :: tl ->
-        iter.data_curr <- tl;
-        Some p
-
-  let iter = iterate next
-  let iter_cache = iterate_cache next []
-end
-
-module A = Array
-
-module Array = struct
-  type data = float array
-  type t = {data:data; mutable pos:int}
-
-  let of_data a = {
-    data = a;
-    pos = -1 (* that way, x starts from 0 *)
-  }
-
-  let reset iter =
-    iter.pos <- -1
-
-  let next iter =
-    if iter.pos >= (A.length iter.data) then
-      None
-    else begin
+let next iter = match iter.data with
+  | List (x, v :: l) ->
+      iter.data <- List (x, l);
+      v
+  | List _ -> raise EOI
+  | Array a ->
+      if iter.pos = Array.length a then raise EOI;
       iter.pos <- succ iter.pos;
-      Some (float iter.pos, A.get iter.data iter.pos)
+      a.(pred iter.pos)
+  | Function f -> begin match Sampler.next f with
+    | None -> raise EOI
+    | Some v -> v
     end
+  | C -> raise EOI
+  | Fortran -> raise EOI
 
-  let iter = iterate next
-  let iter_cache = iterate_cache next []
-end
+let next2 iter = match iter.data2 with
+  | List2 (x, v :: l, x', v' :: l') ->
+      iter.data2 <- List2 (x, l, x', l');
+      v, v'
+  | List2 _ -> raise EOI
+  | Array2 (a, a') ->
+      if iter.pos2 >= min (Array.length a) (Array.length a') then raise EOI;
+      iter.pos2 <- succ iter.pos2;
+      a.(pred iter.pos2), a'.(pred iter.pos2)
+  | Function2 _ -> raise EOI (* Voir le commentaire du début *)
+  | C2 -> raise EOI
+  | Fortran2 -> raise EOI
 
-module Array2 = struct
-  type data = (float * float) array
-  type t = {data:data; mutable pos:int}
+let reset iter = match iter.data with
+  | List (l, _) -> iter.data <- List(l, l)
+  | Array _ -> iter.pos <- 0
+  | Function f -> Sampler.reset f
+  | C  -> ()
+  | Fortran -> ()
 
-  let of_data a = {
-    data = a;
-    pos = -1 (* that way, x starts from 0 *)
-  }
+let reset2 iter = match iter.data2 with
+  | List2 (l, _, l', _) -> iter.data2 <- List2 (l, l, l', l')
+  | Array2 _ -> iter.pos2 <- 0
+  | Function2 _ -> () (* Voir le commentaire du début *)
+  | C2 -> ()
+  | Fortran2 -> ()
 
-  let reset iter =
-    iter.pos <- -1
+let () =
+  let i = of_list [(1., 4.); (4., 2.); (4., 2.)] in
+  let a, b = next i in
+  let c, d = next i in
+  let e, f = next i in
+  Printf.printf "%f %f %f %f %f %f\n" a b c d e f;
+  reset i;
+  let g, h = next i in
+  let k, l = next i in
+  let m, n = next i in
+  Printf.printf "%f %f %f %f %f %f\n" g h k l m n
 
-  let next iter =
-    if iter.pos >= (A.length iter.data) then
-      None
-    else begin
-      iter.pos <- succ iter.pos;
-      Some (A.get iter.data iter.pos)
-    end
 
-  let iter = iterate next
-  let iter_cache = iterate_cache next []
-end
-
-module Bigarray_ = struct
-  open Bigarray
-
-  type 'a datai = (float, float64_elt, 'a) Array1.t
-  type 'a ti = {data:'a datai; mutable pos:int}
-
-  let of_data ba = {
-    data = ba;
-    pos = -1 (* that way, x starts from 0 *)
-  }
-
-  let reset iter =
-    iter.pos <- -1
-
-  let next iter =
-    if iter.pos >= Array1.dim iter.data then None
-    else begin
-      iter.pos <- succ iter.pos;
-      Some (float iter.pos, iter.data.{iter.pos})
-    end
-end
-
-module Bigarray_C = struct
-  include Bigarray_
-  type data = Bigarray.c_layout datai
-  type t = Bigarray.c_layout ti
-
-  let iter = iterate next
-  let iter_cache = iterate_cache next []
-end
-
-module Bigarray_Fortran = struct
-  include Bigarray_
-  type data = Bigarray.fortran_layout datai
-  type t = Bigarray.fortran_layout ti
-
-  let iter = iterate next
-  let iter_cache = iterate_cache next []
-end
-
-module Bigarray2_ = struct
-  open Bigarray
-
-  type 'a datai = (float, float64_elt, 'a) Array2.t
-  type 'a ti = {data:'a datai; mutable pos:int}
-
-  let of_data ba = {
-    data = ba;
-    pos = -1 (* that way, x starts from 0 *)
-  }
-
-  let reset iter =
-    iter.pos <- -1
-
-  let next iter =
-    if iter.pos >= Array2.dim1 iter.data then None
-    else begin
-      iter.pos <- succ iter.pos;
-      Some (iter.data.{iter.pos, 0}, iter.data.{iter.pos, 1})
-    end
-end
-
-module Bigarray2_C = struct
-  include Bigarray2_
-  type data = Bigarray.c_layout datai
-  type t = Bigarray.c_layout ti
-
-  let iter = iterate next
-  let iter_cache = iterate_cache next []
-end
-
-module Bigarray2_Fortran = struct
-  include Bigarray2_
-  type data = Bigarray.fortran_layout datai
-  type t = Bigarray.fortran_layout ti
-
-  let iter = iterate next
-  let iter_cache = iterate_cache next []
-end
-
-module Function = struct
-  include Sampler
-
-  let iter = iterate next
-  let iter_cache = iterate_cache next []
-end
--- src/iterator_public.mli
+++ src/iterator_public.mli
@@ -21,141 +21,23 @@
 (** Iterations on points (internal module). *)
 
 module type T = sig
-  module type Iterator = sig
-    type t
-    type data
+  type ff = float * float
+  type t
+  type t2
 
-    val of_data : data -> t
+  exception EOI
 
-    val next : t -> (float * float) option
-    val reset : t -> unit
-    val iter : (float * float -> unit) -> t -> unit
-    val iter_cache : (float * float -> unit) -> t -> (float * float) list
-  end
+  val of_list : ff list -> t
+  val of_list2 : ff list -> ff list -> t2
+  val of_array : ff array -> t
+  val of_array2 : ff array -> ff array -> t2
+  val of_function : (float -> float * float) -> float -> float -> t
+  val of_function2 : (float -> float * float) ->
+    (float -> float * float) -> float -> float -> t2
 
-  module List : sig
-    include Iterator with type data = float list
-  end
+  val next : t -> ff
+  val next2 : t2 -> ff * ff
 
-  module List2 : sig
-    include Iterator with type data = (float * float) list
-  end
-
-  module Array : sig
-    include Iterator with type data = float array
-  end
-
-  module Array2 : sig
-    include Iterator with type data = (float * float) array
-  end
-
-  module Bigarray_C : sig
-    open Bigarray
-
-    include Iterator with
-      type data = (float, float64_elt, c_layout) Array1.t
-  end
-
-  module Bigarray2_C : sig
-    open Bigarray
-
-    include Iterator with
-      type data = (float, float64_elt, c_layout) Array2.t
-  end
-
-  module Bigarray_Fortran : sig
-    open Bigarray
-
-    include Iterator with
-      type data = (float, float64_elt, fortran_layout) Array1.t
-  end
-
-  module Bigarray2_Fortran : sig
-    open Bigarray
-
-    include Iterator with
-      type data = (float, float64_elt, fortran_layout) Array2.t
-  end
-
-  module Function : sig
-    type strategy = float -> float -> float
-      (** A strategy is a function [f t1 t2] that returns an internal point
-          tm between t1 and t2 which will be used to decide if we need to
-          increment precision or not. *)
-
-    type criterion = float -> float -> float -> float -> float -> float -> bool
-      (** A criterion is a function [f x1 y1 xm ym x2 y2] which returns true
-          if we need to increment precision. *)
-
-    val strategy_midpoint : strategy
-      (** The default strategy: choose the middle point *)
-
-    val strategy_random : strategy
-      (** A strategy that avoids aliasing sampling, but not efficient:
-          chooses randomly a point between t1 and t2 *)
-
-    val strategy_center_random : strategy
-      (** A more efficient strategy that avoids aliasing sampling: chooses
-          randomly a points between t1 and t2 in its 10% center interval *)
-
-    val criterion_none : criterion
-      (** A criterion that tells to never increment precision *)
-
-    val criterion_angle : ?threshold:float -> criterion
-      (** A criterion that tells to increment precision only if the angle
-          xMy is leather than threshold
-
-          @param threshold the minimal angle under which no more precision
-          is needed
-      *)
-
-    val criterion_angle_log : bool -> bool -> ?threshold:float -> criterion
-    (** Same criterion as criterion_angle, but one can tell that the x/y
-        axis is logarithmic, and increment precision in a more wise way *)
-
-    type data = {
-      tlog: bool;
-      min_step: float;
-      nsamples: int;
-      strategy: strategy;
-      criterion: criterion;
-      f: float -> (float * float);
-      t1: float;
-      t2: float
-    }
-
-    type t = {
-      data: data;
-      mutable p: (float * float) option;
-      mutable next: unit -> unit
-    }
-
-    val create : ?tlog:bool -> ?min_step:float -> ?nsamples:int ->
-      ?strategy:strategy -> ?criterion:criterion ->
-      (float -> float * float) -> float -> float -> t
-      (** [create f t1 t2] samples the parametric function f from t1 to t2,
-          returning a list of the points in the sample.
-
-          @param tlog do we need to step in a logarithmic way ?
-
-          @param min_step don't increment precision more than this threshold
-
-          @param nsamples base number of samples wanted (cut the space
-          between t1 and t2 in nsamples fragments of equivalent size,
-          depending on tlog)
-
-          @param strategy a customized strategy, which can be chosen among
-          those in this module
-
-          @param criterion a customized criterion, which can be chosen among
-          those in this module
-      *)
-
-    val of_data : data -> t
-    val reset : t -> unit
-    val next : t -> (float * float) option
-
-    val iter : (float * float -> unit) -> t -> unit
-    val iter_cache : (float * float -> unit) -> t -> (float * float) list
-  end
+  val reset : t -> unit
+  val reset2 : t2 -> unit
 end
