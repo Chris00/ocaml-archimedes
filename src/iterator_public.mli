71afe87690292f86d341c652bdb3f6d68b3e855a
Fabian Pijcke <fabian.pijcke@gmail.com>
2011-07-12 10:15:55 +0200

    Documentation for iterators

--- src/iterator_public.mli
+++ src/iterator_public.mli
@@ -22,25 +22,57 @@
 
 module type T = sig
   type t
+    (** An iterator, created by a "of_*" function, and manipulated through
+        next, reset, iter and iter_cache (see below) *)
+
   exception EOI
+    (** End Of Iterator, the exception raised when next is called on a
+        terminated iterator *)
 
   val of_list : float list -> t
+    (** [of_list l] Transforms a float list into an iterator which will
+        return values (position in [l], value at that position) *)
   val of_array : float array -> t
+    (** [of_array a] Transforms a float array into an iterator which will
+        return values (position in [a], value at that position) *)
   val of_c : (float, Bigarray.float64_elt, Bigarray.c_layout)
     Bigarray.Array1.t -> t
+    (** [of_c b] Transforms a bigarray with a C layout into an iterator
+        which will return values (position in [b], value at that
+        position) *)
   val of_fortran : (float, Bigarray.float64_elt, Bigarray.fortran_layout)
     Bigarray.Array1.t -> t
+    (** [of_fortran b] Transforms a bigarray with a Fortran layout into an
+        iterator which will return values (position in [b], value at that
+        position) *)
 
   val of_list2 : (float * float) list -> t
+    (** [of_list2 l] Transforms a list of float couples into an iterator
+        returning those couples *)
   val of_array2 : (float * float) array -> t
+    (** [array_array2 a] Transforms an array of float couples into an
+        iterator returning those couples *)
   val of_c2 : (float, Bigarray.float64_elt, Bigarray.c_layout)
     Bigarray.Array2.t -> t
+    (** [of_c2 b] Transforms a bigarray of float couples with a C layout
+        into an iterator returning those couples *)
   val of_fortran2 : (float, Bigarray.float64_elt, Bigarray.fortran_layout)
     Bigarray.Array2.t -> t
+    (** [of_fortran2 b] Transforms a bigarray of float couples with a
+        Fortran layout into an iterator returning those couples *)
   val of_function : (float -> float * float) -> float -> float -> t
+    (** [of_function f a b] Create an iterator from a function (R to R x
+        R), refining when necessary to get a smooth curve *)
 
   val next : t -> float * float
+    (** [next iter] returns the next value of [iter] *)
   val reset : t -> unit
+    (** [reset iter] put back [iter] to its initial state *)
   val iter : (float * float -> unit) -> t -> unit
+    (** [iter f iter] apply the function [f] to all values left in the
+        iterator (the iterator won't be resetted !) *)
   val iter_cache : (float * float -> unit) -> t -> (float * float) list
+    (** [iter_cache f iter] apply the function [f] to all values left in
+        the iterator. Those values are stored reversed in a list which is
+        returned *)
 end
