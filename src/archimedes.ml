b9310f07153c0611ecf996905f93e57761e7f358
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2011-07-25 00:18:36 +0200

    Better way of adding all submodules to the Archimedes module.

--- src/archimedes.ml
+++ src/archimedes.ml
@@ -15,21 +15,7 @@
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
    LICENSE for more details. *)
 
-module Color = Archimedes_impl.Color
-module Matrix     = Archimedes_impl.Matrix
-module Backend    = Archimedes_impl.Backend
-module Coordinate = Archimedes_impl.Coordinate
-module Functions  = Archimedes_impl.Functions
-module Pointstyle = Archimedes_impl.Pointstyle
-module Path       = Archimedes_impl.Path
-module Sampler    = Archimedes_impl.Sampler
-module Iterator   = Archimedes_impl.Iterator
-module Tics       = Archimedes_impl.Tics
-module Viewport   = Archimedes_impl.Viewport
-module Arrows     = Archimedes_impl.Arrows
-module Axes       = Archimedes_impl.Axes
-module Plot       = Archimedes_impl.Plot
-module Piechart   = Archimedes_impl.Piechart
+include Archimedes_impl
 
 let init = Viewport.init
 
