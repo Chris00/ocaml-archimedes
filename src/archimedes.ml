3349d15520418d473be55056f331dfade20ea724
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2011-07-24 21:48:13 +0200

    Pack modules in Archimedes_impl so one has full control over the Archimedes interface (especially to put functions at the top level).

---
+++ src/archimedes.ml
@@ -0,0 +1,33 @@
+(* File: archimedes.ml
+
+   Copyright (C) 2011
+
+     Christophe Troestler <Christophe.Troestler@umons.ac.be>
+     WWW: http://math.umons.ac.be/an/software/
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License version 3 or
+   later as published by the Free Software Foundation, with the special
+   exception on linking described in the file LICENSE.
+
+   This library is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
+   LICENSE for more details. *)
+
+module Color = Archimedes_impl.Color
+module Matrix     = Archimedes_impl.Matrix
+module Backend    = Archimedes_impl.Backend
+module Coordinate = Archimedes_impl.Coordinate
+module Functions  = Archimedes_impl.Functions
+module Pointstyle = Archimedes_impl.Pointstyle
+module Path       = Archimedes_impl.Path
+module Sampler    = Archimedes_impl.Sampler
+module Iterator   = Archimedes_impl.Iterator
+module Tics       = Archimedes_impl.Tics
+module Viewport   = Archimedes_impl.Viewport
+module Arrows     = Archimedes_impl.Arrows
+module Axes       = Archimedes_impl.Axes
+module Plot       = Archimedes_impl.Plot
+module Piechart   = Archimedes_impl.Piechart
+
