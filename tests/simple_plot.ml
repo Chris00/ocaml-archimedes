a675bf74ba047fd73922f952c74c23d5a05277ba
Fabian Pijcke <fabian.pijcke@gmail.com>
2011-07-28 12:10:13 +0200

    Sketch grid with box axes

--- tests/simple_plot.ml
+++ tests/simple_plot.ml
@@ -6,6 +6,9 @@ let f x = x *. sin (1. /. x)
 
 let draw bk =
   let vp = A.init ~w ~h ~dirs bk in
-  A.fx vp ~fill:true f (-0.4) 0.4;
+  A.Viewport.set_color vp A.Color.red;
+  A.Viewport.set_mark_size vp 1.;
+  A.fx ~nsamples:2 ~pathstyle:(A.Plot.Points "o") vp ~fill:true f (-0.4) 0.4;
+  A.Viewport.set_color vp A.Color.black;
   A.Axes.box vp;
   A.close vp
