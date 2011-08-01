afc2e0065f8f60b52a56c51f6f1342c5848f1aaf
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2011-08-01 23:09:33 +0200

    New internal module Utils.

---
+++ src/utils.ml
@@ -0,0 +1,20 @@
+(* File: utils.ml
+
+   Copyright (C) 2011
+
+     Christophe Troestler <Christophe.Troestler@umons.ac.be>
+     WWW: http://math.umons.ac.be/an/software/
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License version 3 or
+   later as published by the Free Software Foundation, with the special
+   exception on linking described in the file LICENSE.
+
+   This library is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
+   LICENSE for more details. *)
+
+let is_nan (x: float) = x <> x
+let is_inf y = 1. /. y = 0.
+let is_finite x = neg_infinity < x && x < infinity
