6c424439b33af586b210fbc2af88474681722273
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2011-08-05 12:23:52 +0200

    Adepted the "autofit" test to the new mark names.

--- tests/viewport_autofit.ml
+++ tests/viewport_autofit.ml
@@ -1,7 +1,9 @@
-include Tests_common
+open Tests_common
 
 module V = Archimedes.Viewport
 module P = Archimedes.Path
+
+let description = "Check the automatic determination of ranges"
 
 let draw bk =
   let vp = Archimedes.init ~w ~h ~dirs bk in
@@ -14,21 +16,21 @@ let draw bk =
   P.rectangle p ~x:(-.3.) ~y:2. ~w:6. ~h:6.;
 
   V.set_line_width vp 21.;
-  V.set_color vp Archimedes.Color.red;
+  V.set_color vp Archimedes.Color.orange;
   V.stroke ~path:p vp V.Data;
 
-  V.set_color vp Archimedes.Color.black;
+  V.set_color vp Archimedes.Color.forest_green;
   V.move_to vp ~x:(-3.) ~y:2.;
   V.line_to vp ~x:(-2.) ~y:3.;
   V.stroke vp V.Data;
 
-  V.set_mark_size vp 1.;
+  V.set_mark_size vp 30.;
   V.set_line_width vp 1.;
   V.mark vp 0. 0. "o";
 
   V.set_mark_size vp 50.;
-  V.set_color vp Archimedes.Color.red;
-  V.mark vp (-3.) 2. "X";
+  V.set_color vp Archimedes.Color.silver;
+  V.mark vp (-3.) 2. "x";
 
   V.move_to vp 1. 1.;
   V.line_to vp 2. 2.;
@@ -36,7 +38,7 @@ let draw bk =
   V.stroke vp V.Data;
 
   V.rectangle vp 0. 0. 1. 1.;
-  V.set_color vp Archimedes.Color.yellow;
+  V.set_color vp Archimedes.Color.red;
   V.stroke vp V.Graph;
 
   V.set_color vp Archimedes.Color.black;
