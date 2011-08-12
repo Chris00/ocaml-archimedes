7e117da9794bc9e7ff8497bb5c446a2894d0f131
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2011-08-12 14:16:34 +0200

    New example.

--- examples/taylor.ml
+++ examples/taylor.ml
@@ -1,44 +1,43 @@
-open Archimedes
-module P = Plot.Array
+module A = Archimedes
 
 let tsin d =
   (* Compute the Taylor expansion of sin of order 2d+1 *)
-  let d = max d 0 in
+  if d < 0 then invalid_arg "tsin";
   let fac = ref 1.
   and s = ref 1. in
-  let p = Array.init (d+1)
-            (fun i ->
-               let c = !s /. !fac in
-               let i2 = 2. *. float(i+1) in
-               let () =
-                 fac := !fac *. i2 *. (i2 +. 1.);
-                 s := -. !s in
-               c) in
+  let p = Array.init (d+1) begin fun i ->
+    let c = !s /. !fac in
+    let i2 = 2. *. float(i+1) in
+    fac := !fac *. i2 *. (i2 +. 1.);
+    s := -. !s;
+    c
+  end in
   fun x ->
     (* Horner eval of the poly *)
     let x2 = x *. x
     and y = ref 0. in
-    for i = d downto 0 do
-      y := p.(i) +. !y *. x2
-    done;
+    for i = d downto 0 do y := p.(i) +. !y *. x2 done;
     !y *. x
 
 let () =
-  try
-    let p = P.make "graphics hold" 600. 600. in
-
-    let b = 9. in
+    let xmax = 9. in
     let f = Array.init 10 tsin in
-    P.f p sin 0. b;
-    P.set_color p Color.blue;
-    P.yrange p (-3.) 3.;
+
+    let vp = A.init (try A.backend_of_filename Sys.argv.(1) with _ -> [])
+      ~w:600. ~h:600. in
+    A.Axes.box vp;
+    A.Viewport.yrange vp (-3.) 3.;
+    A.set_color vp A.Color.blue;
+    A.set_line_width vp 1.;
+    (* P.yrange p (-3.) 3.; *)
     let len = Array.length f - 1 in
     for i = 0 to len do
-      let c = (float i /. float len) in
-      P.set_color p (Color.rgb c 0. (1. -. c));
-      P.f p f.(i) 0. b;
+      let c = float i /. float len in
+      A.set_color vp (A.Color.rgb c 0. (1. -. c));
+      A.fx vp f.(i) 0. xmax;
     done;
+    A.set_color vp A.Color.green;
+    A.set_line_width vp 2.;
+    A.fx vp sin 0. xmax;
 
-    P.close p
-  with Backend.Error e ->
-    Printf.printf "%s\n" (Backend.string_of_error e)
+    A.close vp
