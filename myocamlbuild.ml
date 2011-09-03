33645118814a3ec0a556b5f719124b789382b574
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2011-09-03 17:24:18 +0200

    Added rules to build cmxs for ocaml ≤ 3.11.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -479,6 +479,20 @@ Ocamlbuild_plugin.dispatch
       dep ["file:src/plot.ml"] ["src" / "plot_arr.ml"];
       dep ["file:src/path.ml"] ["src" / "path_arr.ml"];
 
+      (* ocamlbuild does not support cmxs before 3.12.0.  Quick hack. *)
+      let version_major, version_minor =
+        Scanf.sscanf Sys.ocaml_version "%i.%i" (fun mj mi -> mj, mi) in
+      if version_major <= 3 && version_minor <= 11 then (
+        rule "cmxa -> cmxs" ~dep:"%.cmxa" ~prod:"%.cmxs"
+          begin fun env build ->
+            let cmxa = env "%.cmxa" and cmxs = env "%.cmxs" in
+            Cmd(S [!Options.ocamlopt; A "-shared"; A "-linkall";
+                   T (tags_of_pathname cmxs ++ "library" ++ "native" ++ "ocaml"
+                      ++ "link" ++ "shared");
+                   P cmxa; A "-o"; Px cmxs])
+          end;
+      )
+
     | _ -> ()
     end;
   ]);;
