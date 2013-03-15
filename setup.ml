7d9d85d69b26e349627912e3d10049e84acc2a97
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2013-03-15 22:29:23 +0100

    setup: env. var. OCAML_TOPLEVEL_PATH detected outside oasis block

--- setup.ml
+++ setup.ml
@@ -1,9 +1,8 @@
-(* OASIS_START *)
-
 let () =
   try Topdirs.dir_directory (Sys.getenv "OCAML_TOPLEVEL_PATH")
   with Not_found -> ();;
 
+(* OASIS_START *)
 (* DO NOT EDIT (digest: 7f47a529f70709161149c201ccd90f0b) *)
 #use "topfind";;
 #require "oasis.dynrun";;
